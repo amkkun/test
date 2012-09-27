6c389cb9473cad504fb9c54dfa776448955c6c18
amkkun <amkkun@gmail.com>
2012-09-27 18:24:47 +0900

    Update mergesort.hs

--- mergesort.hs
+++ mergesort.hs
@@ -11,4 +11,6 @@ mergesort xs = merge (mergesort left) (mergesort right)
 merge :: Ord a => [a] -> [a] -> [a]
 merge [] ys = ys
 merge xs [] = xs
-merge (x:xs) (y:ys) = if x <= y then x : merge xs (y:ys) else y : merge (x:xs) ys 
+merge (x:xs) (y:ys) = if x <= y 
+                        then x : merge xs (y:ys) 
+                        else y : merge (x:xs) ys 
